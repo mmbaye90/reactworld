4bf02f51cd45245431674825036ee91e38391eb8
Mamadou Mbaye <81474777+mmbaye90@users.noreply.github.com>
2021-12-10 00:58:11 +0100

    Ajout css au logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,9 +1,13 @@
 import React from "react";
 import logoreact from "../assets/logoreact.png"
+import "../styles/logo.css"
 const Logo=()=>{
     return(
-        <div>
+        <div className="cont-log">
             <img src={logoreact} alt={logoreact} />
+            <h3 className="titre">
+                React World
+            </h3>
         </div>
     )
 }
