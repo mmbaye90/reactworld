b36761707cb9f57ff328e13df47c25621e35e560
Mamadou Mbaye <81474777+mmbaye90@users.noreply.github.com>
2021-12-10 01:23:19 +0100

    Ajout style sur la navigation des pages About & Home

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import "../styles/navigation.css"
 
 const Navigation = () => {
     return (
-        <div>
-             <NavLink exact to="/" >Accueil</NavLink>
-             <NavLink exact to="/about">About</NavLink>
+        <div className="navigation">
+             <NavLink exact to="/" activeClassName="lien-active" className="lien">Accueil</NavLink>
+             <NavLink exact to="/about"activeClassName="lien-active"className="lien">About</NavLink>
         </div>
        
 
