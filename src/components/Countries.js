948fcbc9f23f2a945d9ba2e5d61d9aa02ab89dc9
Mamadou Mbaye <81474777+mmbaye90@users.noreply.github.com>
2021-12-11 22:03:46 +0100

    $ git commit ajout des pays du monde avec input de type range pour permettre au user de ranger les pays par ordere croissant deopulations

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,29 +1,49 @@
 import React from 'react';
 import axios from 'axios';
-import{useState,useEffect} from "react"
-import Card from './Card';
+import { useState,useEffect } from 'react/cjs/react.development';
 
 const Countries = () => {
-    const[data,setData]=useState([]);
+    const[data,setData] = useState([]);
     const url = "https://restcountries.com/v3.1/all?fields=flags,name,population,capital,region";
+    const [getApi,setGetApi] = useState(true);
+    const[rangeValue,setRangeValue] = useState(3);
+    const[dataTransformed,setDataTransformed] = useState([])
 
     useEffect(() => {
-        axios
+        if(getApi){
+            axios
         .get(url)
-        .then(res=>{
-            setData(res.data)
-        })
-        
-    },[]);
+        .then(res =>setData(res.data))
+        setGetApi(false)
+        };
+        const arrtransforme=()=>{
+            const objcData = Object.keys(data).map((i)=>data[i]);
+            const arr = objcData.sort((a,b)=>{
+                return b.population - a.population;
+            })
+            arr.length = rangeValue
+            setDataTransformed(arr)
+        }
+        arrtransforme()
+       
+        //     const datObject = Object.keys(data).map((i)=>data[i]);
+        //     const arr = datObject
+        // }
+        // dataConverti()
+    }, [data,getApi,rangeValue]);
+
     return (
-        <div className="countries">
+        <div>
+            <div className="sorcontener">
+                <input type="range" min="1" max="250" onChange={(e)=>setRangeValue(e.target.value)} />
+            </div>
             <ul>
-                {data.map(country=>(
-                    <Card country ={country}/>
+                {dataTransformed.map((country)=>(
+                    <li key={country.name.common}>{country.name.common}</li>
                 ))}
             </ul>
         </div>
     );
-};
+}
 
 export default Countries;
