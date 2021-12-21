a56cac6754c51e67f9e5e87fdb992b04ba26bb51
Mbaye <mmbaye1@hotmail.fr>
2021-12-21 16:08:04 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import { BrowserRouter,Switch,Route } from "react-router-dom";
+import { BrowserRouter,Route } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Notfound from "./pages/Notfound";
 
 function App() {
   return (
-  <BrowserRouter>
-  <Switch>
+  <BrowserRouter basename={window.location.pathname || ''}>
     <Route  path="/" exact component={Home} />
     <Route  path="/about" exact component={About} />
 
     <Route  component={Notfound} />
-  </Switch>
   </BrowserRouter>
   );
 }
